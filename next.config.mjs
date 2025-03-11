35782af2bad4deae39c19cac2c80658d2a3e385e
Norman Sobczyk <wgwcompany@duck.com>
2025-03-11 16:50:19 +0000

    update

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+setupDevPlatform().catch(console.error);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
