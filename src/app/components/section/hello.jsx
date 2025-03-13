42c031009aead3bacc98405b910a072a97fdd3d9
_niezle_ziolko <wgwcompany@duck.com>
2025-03-13 21:41:45 +0100

    update

--- src/app/components/section/hello.jsx
+++ src/app/components/section/hello.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Hello() {
   return (
     <section className='hello'>
@@ -5,7 +7,7 @@ export default function Hello() {
       
       </div>
       <div className='right'>
-        
+        <Image src='/memoji.webp' width={150} height={180} quality={100} alt="memoji" />
       </div>
     </section>
   );
