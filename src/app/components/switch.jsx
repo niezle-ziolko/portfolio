0878c46649f4aa63531e06495e5f81c143c619f9
_niezle_ziolko <wgwcompany@duck.com>
2025-03-13 07:35:59 +0100

    update

--- src/app/components/switch.jsx
+++ src/app/components/switch.jsx
@@ -7,19 +7,21 @@ export default function Switch() {
   const { isDarkMode, setIsDarkMode } = useTheme();
 
   return (
-    <div class="container">
-        <div class="circle"></div>
-        <label for="switch" class="switch">
-            <input type="checkbox" id="switch" class="hidden" />
-            <div class="switch-body">
-                <div class="arrow">
-                    <div class="arrow-inner"></div>
-                </div>
-                <div class="skew-box left"></div>
-                <div class="skew-box right"></div>
+    <div class='custom-container'>
+      <div class='custom-content'>
+        <div class='custom-circle' />
+        <label htmlFor='switch' class='custom-label'>
+          <input type='checkbox' id='switch' class='custom-input' checked={isDarkMode} onChange={() => setIsDarkMode(!isDarkMode)} />
+          <div class='custom-switch'>
+            <div class='custom-arrow'>
+              <div class='custom-arrow-inner' />
             </div>
+            <div class='custom-skew-left' />
+            <div class='custom-skew-right' />
+          </div>
         </label>
-        <div class="small-bar"></div>
+        <div class='custom-line' />
+      </div>
     </div>
   );
 };
