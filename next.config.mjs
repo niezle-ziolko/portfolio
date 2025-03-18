a5878692f10ef0fd2739fbb1987f45e27b9ea0d9
_niezle_ziolko <wgwcompany@duck.com>
2025-03-18 20:19:26 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 setupDevPlatform().catch(console.error);
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: [
-    '*.gitpod.io',
-    '3000-niezleziolko-portfolio-glfvzhmg3a7.ws-eu118.gitpod.io'
-  ]
-};
+const nextConfig = {};
 
 export default nextConfig;
