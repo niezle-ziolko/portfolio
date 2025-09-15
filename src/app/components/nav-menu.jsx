2fa0f5cb1696b6f08cbd02c9c365ba2048e2d601
Norman Sobczyk <wgwcompany@duck.com>
2025-09-15 07:45:18 +0000

    update

--- src/app/components/nav-menu.jsx
+++ src/app/components/nav-menu.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-import Icon from "./icon";
+import Icon from "./SvgIcon";
 
 import ThemeButton from "./theme-button";
 
@@ -14,7 +14,7 @@ export default function NavMenu() {
       <ul className={`flex flex-col w-auto items-start justify-start transition-height duration-200 md:flex-row md:items-center md:justify-between ${isOpen ? "h-screen" : "h-11"}`}>
         <li>
           <ul className="h-11 flex items-center justify-between">
-            <li className={`u10 ${isOpen ? "opacity-0" : "opacity-100"}`}>
+            <li className={`u10 u13 ${isOpen ? "opacity-0" : "opacity-100"}`}>
               <Icon
                 width={size}
                 height={size}
@@ -25,7 +25,12 @@ export default function NavMenu() {
             <li className="w-full block md:hidden">
               <div className="flex justify-end items-center">
                 <label className="relative cursor-pointer hover:text-white">
-                  <input type="checkbox" id="menu" className="absolute opacity-0 z-20 peer" onClick={() => setIsOpen((prev) => !prev)}/>
+                  <input
+                    id="menu"
+                    type="checkbox"
+                    className="absolute opacity-0 z-20 peer"
+                    onClick={() => setIsOpen((prev) => !prev)}
+                  />
                   <span className="u11 transition-rotate peer-checked:translate-y-[4.5px] peer-checked:rotate-45" />
                   <span className="u11 transition-rotate peer-checked:translate-y-[-4px] peer-checked:-rotate-45" />
                 </label>
@@ -56,30 +61,48 @@ export default function NavMenu() {
         <li className="md:w-2/5">
           <ul className="u12 flex mx-0 py-5 md:py-0 justify-between">
             <li className="u13">
-              <Link href="tel:+48733196208" alt="Phone number" target="_blank">
+              <Link
+                className="u13"
+                target="_blank"
+                alt="Phone number"
+                href="tel:+48733196208"
+              >
                 <Icon
                   width={size}
                   height={size}
+                  className="u14"
                   alt="Phone icon"
                   src="/phone.svg"
                 />
               </Link>
             </li>
             <li className="u13">
-              <Link href="mailto:wgwcompany@duck.com" alt="Email" target="_blank">
+              <Link
+                alt="Email"
+                className="u13"
+                target="_blank"
+                href="mailto:wgwcompany@duck.com"
+              >
                 <Icon
                   width={size}
                   height={size}
+                  className="u14"
                   alt="Email icon"
                   src="/email.svg"
                 />
               </Link>
             </li>
             <li className="u13">
-              <Link alt="Github" target="_blank" href="https://github.com/niezle-ziolko">
+              <Link
+                alt="Github"
+                target="_blank"
+                className="u13"
+                href="https://github.com/niezle-ziolko"
+              >
                 <Icon
                   width={size}
                   height={size}
+                  className="u14"
                   alt="Github icon"
                   src="/github.svg"
                 />
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
-  const size = 1000;
 
   return (
     <div>
-      <Image
-        width={size}
-        height={size}
-        alt="Avatar"
-        src="/avatar.svg"
-      />
+      
     </div>
   );
 };
