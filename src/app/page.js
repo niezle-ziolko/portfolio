102684e9effe117da2fddfb5b844dd78df0b8662
_niezle_ziolko <wgwcompany@duck.com>
2025-10-12 20:35:36 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import About from "components/about";
 import Stack from "components/stack";
 import Laptop from "components/laptop";
 import Skills from "components/skills";
-import Projects from "components/projects";
 
 export default function Home() {
   return (
@@ -10,17 +9,17 @@ export default function Home() {
       <section className="bg-black">
         <Laptop />
       </section>
+
       <section id="about" className="overflow-hidden">
         <About />
       </section>
+
       <section id="stack">
         <Stack />
       </section>
+
       <section id="skills">
         <Skills />
-      </section>
-      <section id="about" className="overflow-hidden">
-        <About />
       </section>
     </div>
   );
